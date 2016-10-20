883e02013741deb958e86b93838ad70464b53fbb
Nicholas Helke <nicholas.helke@kaldorgroup.com>
2016-10-20 10:56:31 +0100

    Don't ask for wall clock time unnecessarily

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 )
 
 func main() {
-	delay, err := time.ParseDuration(os.Args[1])
-	if err != nil || len(os.Args) != 2 {
+	if len(os.Args) != 2 {
 		panic("Expected a single duration argument")
+	}
+	delay, err := time.ParseDuration(os.Args[1])
+	if err != nil {
+		panic(err)
 	}
 	now := time.Now()
 	t := now.Add(delay)
-	fmt.Printf("%v %v: should wake at %v in %v\n", delay, now, t, -time.Since(t))
-	<-time.After(-time.Since(t))
+	fmt.Printf("%v %v: should wake at %v in %v\n", delay, now, t, delay)
+	<-time.After(delay)
 	nownow := time.Now()
 	fmt.Printf("%v %v: did wake with delta %v\n", delay, nownow, nownow.Sub(t))
 }
